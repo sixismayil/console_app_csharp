b123fe4b35f1be2fd264f96bc7d8173a0f619acf
shixismayil <sixismayil@gmail.com>
2023-07-08 21:49:03 +0400

    complex_creature

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 1.Simple Creature
 
 ![Simple Creature](simple_creature.png)
+
+2.Complex Creature
+
+![Complex Creature](complex_creature.png)
---
+++ complex_creature.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c_      __");
+            Console.WriteLine("   \\\\__|__|__");
+            Console.WriteLine("     \\( o_o)");
+            Console.WriteLine("       > ~  >");
+            Console.WriteLine("      /    / \\\\");
+            Console.WriteLine("     /    /   \\\\");
+            Console.WriteLine("     \\    )   c/");
+            Console.WriteLine("     /   /");
+            Console.WriteLine("    /   /|");
+            Console.WriteLine("   (   ( \\");
+            Console.WriteLine("   |   |  \\");
+            Console.WriteLine("   |  / \\  )");
+            Console.WriteLine("   |  |  ) |");
+            Console.WriteLine("   /  )(_/   ");
+            Console.WriteLine("  (_ /");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
