3fc836aaf11e8081eac171ecdd55e71c84f151af
shixismayil <sixismayil@gmail.com>
2023-07-08 10:11:15 +0400

    simple_creature

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # console_app_csharp
+
+1.Simple Creature
+
+![Simple Creature](simple_creature.png)
---
+++ simple_creator.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("  .-. ");
+            Console.WriteLine(" (o o) ");
+            Console.WriteLine(" | O | ");
+            Console.WriteLine(" |   | ");
+            Console.WriteLine("  '~' ");
+
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
